e95100456e20fc69c8537cf7957a536433d46b92
sabilimaulana <spacysbl.learn@gmail.com>
2022-04-17 20:07:04 +0700

    done: Section 3

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,16 +7,56 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
 
-    function wave() public {
-        totalWaves++;
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
+    constructor() payable {
+        console.log("I AM SMART CONTRACT. POG.");
+    }
+
+    function wave(string memory _message) public {
+        totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
